ef3a743105cad238854fbec0ccb075307c5b6b9e
khoatiendao <88887127+khoatiendao@users.noreply.github.com>
2025-09-24 19:30:31 +0700

    Update skills section

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+// ======================== Open Resume with Popup ========================
 function openPopup() {
-  document.getElementById("resumePopup").style.display = "flex";
+  document.getElementById('resumePopup').style.display = 'flex';
 }
 
 function closePopup() {
-  document.getElementById("resumePopup").style.display = "none";
+  document.getElementById('resumePopup').style.display = 'none';
 }
 
-window.onclick = function(event) {
-  const popup = document.getElementById("resumePopup");
+window.onclick = function (event) {
+  const popup = document.getElementById('resumePopup');
   if (event.target === popup) {
     closePopup();
   }
-}
+};
+
+// ========================== Responsive ==============================
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+  document.querySelector('.menu').classList.toggle('active');
+});
